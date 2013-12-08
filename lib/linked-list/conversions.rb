5a96ba4c094ab4e6f27e447605c69a8210c372fc
Yury Velikanau <yury.velikanau@gmail.com>
2013-12-07 19:12:42 -0800

    Introduce List() conversion function.

--- lib/linked-list/conversions.rb
+++ lib/linked-list/conversions.rb
@@ -17,5 +17,26 @@ module LinkedList
         Node.new(args.first)
       end
     end
+
+    # +List()+ tries to conver its argument to +List+ object by first calling
+    # +#to_list+, if that is not availabe and its argument is an array (or can
+    # be convertd into array with +#to_ary+) then it will instantiate a new
+    # +List+ object making nodes from array elements. If none above applies,
+    # then a new +List+ will be instantiated with one node holding argument
+    # value.
+    #
+    # == Returns:
+    # New +List+ object.
+    #
+    def List(*args)
+      case args.first
+      when ->(arg) { arg.respond_to?(:to_list) }
+        args.first.to_list
+      when ->(arg) { arg.respond_to?(:to_ary) }
+        args.first.to_ary.each_with_object(List.new) { |n, l| l.push(Node(n)) }
+      else
+        List.new.push(Node(args.first))
+      end
+    end
   end
 end
--- test/conversions_test.rb
+++ test/conversions_test.rb
@@ -12,4 +12,21 @@ describe LinkedList::Conversions do
         LinkedList::Conversions.Node('foo')
     end
   end
+
+  describe 'List()' do
+    it 'returns self if list is given' do
+      list = create_list
+      assert_equal LinkedList::Conversions.List(list), list
+    end
+
+    it 'returns list with nodes made from array' do
+      list = LinkedList::Conversions.List([1, 2])
+      assert_equal [1, 2], [list.head.data, list.tail.data]
+    end
+
+    it 'returns new list with one node' do
+      list = LinkedList::Conversions.List('foo')
+      assert_equal 'foo', list.head.data
+    end
+  end
 end
