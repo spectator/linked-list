7ac554307da37ea4fc65608ad0a8126deed488bb
Yury Velikanau <yury.velikanau@gmail.com>
2013-12-07 19:12:14 -0800

    Update Node() doc.

--- lib/linked-list/conversions.rb
+++ lib/linked-list/conversions.rb
@@ -2,6 +2,13 @@ module LinkedList
   module Conversions
     module_function
 
+    # +Node()+ tries to convert its argument to +Node+ object by first calling
+    # +#to_node+, if that is not availabe then it will instantiate a new +Node+
+    # object.
+    #
+    # == Returns:
+    # New +Node+ object.
+    #
     def Node(*args)
       case args.first
       when ->(arg) { arg.respond_to?(:to_node) }
