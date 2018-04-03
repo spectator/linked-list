a8c111e0b1601c833478d832c4c547f4a7953170
Yury Velikanau <yury.velikanau@gmail.com>
2018-04-02 21:32:17 -0700

    Add frozen_string_literal magic comment.

--- lib/linked-list.rb
+++ lib/linked-list.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'linked-list/conversions'
 require 'linked-list/node'
 require 'linked-list/list'
--- lib/linked-list/version.rb
+++ lib/linked-list/version.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Linked
   module List
     VERSION = '0.0.9'
